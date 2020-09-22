3788c3ab50ddd7214bcd7db8da7a415aa55e75e8
Jennie Seo <jennie.seo707@sccdsb.net>
2020-09-22 14:08:30 -0400

    Made edits to introduce myself

--- AboutMe.java
+++ AboutMe.java
@@ -1,24 +1,28 @@
 /*
- * Programmer:
- * Date:
- * Purpose:
+Programmer: Jennie
+Date: September 22, 2020
+Purpose: Stores information about me in variables and prints them out
  */
 
 public class AboutMe {
     public static void main (String [] args) {
         // Declaring the variables
-        // TODO: Write the code the declares the variables
+    	String myName = "Jennie";
+    	int myAge = 16;
+    	double myHeightInMetres = 1.59;
 
         // Print the values
-        System.out.println("Hi, my name is " + myName);
+        System.out.println("Hi, my name is " + myName + ".");
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
 
         // Change the variables
-        // TODO: Write the code that changes the variables
+        myAge = 20;
+        myHeightInMetres = 1.65;
 
         // Print the new values
         System.out.println("Some time passes...");
-        // TODO: Write the last two print statements
+        System.out.println("I am now " + myAge + " years old.");
+        System.out.println("I have grown and am " + myHeightInMetres + " m tall.");
     }
 }
